5a280d990d0a2cc793bdeec5f845cdb1e37271d0
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-11-21 11:35:39 +0100

    repush

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :games do
+    resources :rents, only: [:new, :create]
+  end
 end
