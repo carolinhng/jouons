94c7271e9022fff8473b74e886bb2d687ddf690e
Octave Dubois <octave.dubois@hotmail.fr>
2023-11-21 15:53:03 +0100

    controller New + Create + Edit + Update + Destroy faits

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,17 +6,41 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path, status: :see_other
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :price, :player_number, :games_durantion, :availability)
+  end
+
 end
