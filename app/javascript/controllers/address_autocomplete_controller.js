a6cc4d861e6f38b703342ec25bea7d9ced3a3051
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-11-23 17:20:37 +0100

    autocomplete

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -13,9 +13,19 @@ export default class extends Controller {
       types: "country,region,place,postcode,locality,neighborhood,address"
     })
     this.geocoder.addTo(this.element)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
   }
 
   disconnect() {
     this.geocoder.onRemove()
+  }
+
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
   }
 }
