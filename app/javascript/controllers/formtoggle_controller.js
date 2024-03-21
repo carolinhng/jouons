28866781c3e2876b695e06962dc9321a825a4002
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2024-03-21 11:22:43 +0100

    test

--- app/javascript/controllers/formtoggle_controller.js
+++ app/javascript/controllers/formtoggle_controller.js
@@ -21,19 +21,19 @@ export default class extends Controller {
       .then((data) => {
       })
 
-    this.togglableElementTargets.forEach(target => {
-      if (target.innerText === " Rendre mon jeu disponible") {
-        target.innerHTML = `<i class="fa-solid fa-xmark ms-2"></i> Rendre mon jeu indisponible`;
-        target.classList.remove("btn-danger");
-        target.classList.add("btn-success")
-      }
-      else {
-        target.innerHTML = `<i class="fa-solid fa-bolt"></i> Rendre mon jeu disponible`
-        target.classList.remove("btn-success")
-        target.classList.add("btn-danger")
-      }
-    });
-
+      this.togglableElementTargets.forEach(target => {
+        if (target.innerText === " Rendre mon jeu disponible") {
+          target.innerHTML = `<i class="fa-solid fa-xmark ms-2"></i> Rendre mon jeu indisponible`;
+          target.classList.remove("btn-danger");
+          target.classList.add("btn-success")
+        }
+        else {
+          target.innerHTML = `<i class="fa-solid fa-bolt"></i> Rendre mon jeu disponible`
+          target.classList.remove("btn-success")
+          target.classList.add("btn-danger")
+        }
+      });
+  
 
       // this.buttonTarget.innerText = "Click me!";
       // this.buttonTarget.removeAttribute("disabled");
